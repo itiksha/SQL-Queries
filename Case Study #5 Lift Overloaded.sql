b7a175db017f1a8f4cb345fc4a1bd8d71370b24d
itiksha <104241200+itiksha@users.noreply.github.com>
2024-06-09 18:21:00 +0530

    Add files via upload

--- Case Study #5 Lift Overloaded.sql
+++ Case Study #5 Lift Overloaded.sql
@@ -30,6 +30,70 @@ INSERT INTO lift_passengers (passenger_name, weight_kg, lift_id) VALUES
 
 
 -- Que For each lift find the comma separated list of people who can be accomodated. The comma separated list should have people in the order of their weight in increasing order
+
+-- 1st Warm up
+select  
+p.passenger_name , 
+p.weight_kg 
+from lifts l 
+join 
+lift_passengers p 
+on l.id = p.lift_id;
+
+--  2nd Warm up
+with Eighty as ( 
+select  
+weight_kg 
+from 
+lift_passengers
+where weight_kg = 80
+)
+select  
+passenger_name , 
+weight_kg 
+from 
+lift_passengers
+where weight_kg > (select  
+weight_kg 
+from 
+lift_passengers
+where weight_kg = 80);
+
+-- Third Warm up 
+select  p1.passenger_name , p1.weight_kg
+from  lift_passengers p1
+join lift_passengers p2 on 
+p1.weight_kg > p2.weight_kg 
+where p2.weight_kg = 83 ;
+
+
+-- Fourth Warm up(which throws an error)
+select p2.passenger_name, p2.weight_kg 
+from lift_passengers p2
+join lift_passengers p1 on 
+p2.weight_kg > p1.weight_kg 
+where p1.weight_kg = 49;
+and p2.weight_kg < 95;
+
+
+-- Fifth Warm up (which return blank)
+SELECT p2.passenger_name, p2.weight_kg 
+FROM lift_passengers p2
+JOIN lift_passengers p1 ON p2.weight_kg > p1.weight_kg 
+WHERE p1.weight_kg = 49;
+
+-- Sixth Warm up (which actually make some sense)
+select 
+l.id,
+lp.weight_kg,
+l.capacity_kg,
+sum(lp.lift_id) over(partition by l.id order by lp.weight_kg 
+rows between unbounded preceding and current row) as running_sum
+from lifts l 
+inner join lift_passengers lp on
+l.id = lp.lift_id;
+
+-- Final Result (which actually return desired result)
 -- Common Table Expression (CTE) named "Passengers"
 -- This CTE calculates the running sum of passenger weights for each lift
 WITH Passengers AS (
